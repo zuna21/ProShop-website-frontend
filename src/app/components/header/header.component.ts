844dee80fdb6accbd1bd63c385859a8f797ac1e1
zuna21 <adir.zunic@gmail.com>
2022-11-19 09:47:53 +0100

    user in navbar and logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
 
-  constructor() { }
+  public isLoggedIn: boolean = false;
+  public userName: string = null;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem('userInfo')) {
+      this.isLoggedIn = true;
+      this.userName = JSON.parse(localStorage.getItem('userInfo')).name;
+    }
+  }
+
+  ngDoCheck(): void {
+    if(localStorage.getItem('userInfo')) {
+      this.isLoggedIn = true;
+      this.userName = JSON.parse(localStorage.getItem('userInfo')).name;
+    } else {
+      this.isLoggedIn = false;
+      this.userName = null;
+    }
+  }
+
+
+  onLogout() {
+    this.isLoggedIn = false;
+    this.userName = null;
+    localStorage.removeItem('userInfo');
+    this.router.navigate(['home']);
   }
 
 }
